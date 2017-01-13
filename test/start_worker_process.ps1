98740bbde2857cd309447e33f13d6b1905d76853
Ismael Apolinario <ismaelapolinario@MacBook-Pro-de-Ismael.local>
2017-01-13 00:34:08 -0200

    Improve start-process command call

--- test/start_worker_process.ps1
+++ test/start_worker_process.ps1
@@ -1,2 +1,2 @@
 echo "Starting the Worker Process in Background..."
-Start-Process C:\CI\CIAssignment\src\FourC.Worker.Backend\bin\release\net461\win7-x64\publish\FourC.Worker.Backend.exe -NoNewWindow  
+Start-Process -WorkingDirectory "C:\CI\CIAssignment\src\FourC.Worker.Backend\bin\release\net461\win7-x64\publish" -FilePath "FourC.Worker.Backend.exe" -NoNewWindow 
