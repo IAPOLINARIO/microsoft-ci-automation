da32f733e52ad82339e4ef3ad3feb23730c99dff
Ismael Apolinario <ismaelapolinario@MacBook-Pro-de-Ismael.local>
2017-01-15 02:36:24 -0200

    Resolving database create issue

--- deploy/create_database.ps1
+++ deploy/create_database.ps1
@@ -5,8 +5,12 @@ if(-not($sql_instance)) {
     throw "SQLEXPRESS not found"
 }
 
+
+Write-Host "Set Database to Single User Mode"
+Invoke-Sqlcmd -Query "IF EXISTS(select * from sys.databases where name='Worker') ALTER DATABASE Worker SET SINGLE_USER WITH ROLLBACK IMMEDIATE" -ServerInstance (".\SQLEXPRESS") -Database "Master"
+
 Write-Host "Drop database if already exists..."
-Invoke-Sqlcmd -Query "IF EXISTS(select * from sys.databases where name='Worker') DROP DATABASE Worker" -ServerInstance (".\" + $sql_instance) -Database "Master"
+Invoke-Sqlcmd -Query "IF EXISTS(select * from sys.databases where name='Worker') DROP DATABASE Worker" -ServerInstance (".\" + $sql_instance) -Database "Master" 
 
 Write-Host "Recreating Database..."
 Invoke-Sqlcmd -InputFile ($database_script_path + "dbo.Work.sql") -ServerInstance (".\" + $sql_instance)
