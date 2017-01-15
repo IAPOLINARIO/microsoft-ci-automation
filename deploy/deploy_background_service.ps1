552eab51377b2421018b6687d87ddaabd3ed1477
Ismael Apolinario <ismaelapolinario@MacBook-Pro-de-Ismael.local>
2017-01-15 19:11:49 -0200

    Fix process name issue

--- deploy/deploy_background_service.ps1
+++ deploy/deploy_background_service.ps1
@@ -1,8 +1,10 @@
-$backend_process = Get-Process -Name $backend_sercice_file
+$backend_process_name = $backend_service_file.Substring(0, $backend_service_file.LastIndexOf('.'))
+$backend_process = Get-Process -Name $backend_process_name
+
 
 Write-Host "Checking if another process are running with the same name..."
 if($backend_process){
-    Stop-Process -Name $backend_sercice_file
+    Stop-Process -Name $backend_process_name
 }
 
 Write-Host "Starting the Worker Process in Background..."
