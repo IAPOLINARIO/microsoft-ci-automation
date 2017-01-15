e994ea099826edf5d161878a8d9036e8fc1fd104
Ismael Apolinario <ismaelapolinario@MacBook-Pro-de-Ismael.local>
2017-01-15 17:03:27 -0200

    Add script to deploy api on IIS

--- deploy/setup_app_pool.ps1
+++ deploy/setup_app_pool.ps1
@@ -4,8 +4,7 @@ Import-Module WebAdministration
 Write-Host "Checking if Pool already exists..."
 $api_pool = Get-ChildItem -Path "IIS:\AppPools" | Where {$_.Name -match $api_site_name} | Select Name -First 1
 
-if(-not ($api_pool))
-{
+if(-not ($api_pool)){
     Write-Host "Creating new App Pool for API..."
     New-Item –Path $iis_app_pool_path
 }
