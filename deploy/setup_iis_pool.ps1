332c76f8364bace40f80aaedbd93b003c2695333
Ismael Apolinario <ismaelapolinario@MacBook-Pro-de-Ismael.local>
2017-01-15 18:06:55 -0200

    Fix App Pool problem

--- deploy/setup_iis_pool.ps1
+++ deploy/setup_iis_pool.ps1
@@ -4,8 +4,7 @@ Import-Module WebAdministration
 Write-Host "Checking if Pool already exists..."
 $api_pool = Get-ChildItem -Path $base_iis_app_pool_path | Where { $_.Name -match $api_site_name } | Select Name -First 1
 
-if(-not($api_pool)){
-    New-Item –Path $iis_api_pool_path
+if(!($api_pool)){
+    New-Item -Path $iis_api_pool_path
 }
-
 Set-ItemProperty -Path $iis_api_pool_path -Name managedRuntimeVersion -Value ''
