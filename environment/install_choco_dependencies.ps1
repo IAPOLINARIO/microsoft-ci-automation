d5bd82d5010abe7387ea8619dcf789df7bd790af
Ismael Apolinario <ismaelapolinario@MacBook-Pro-de-Ismael.local>
2017-01-16 19:22:13 -0200

    Fix the environment path variable name

--- environment/install_choco_dependencies.ps1
+++ environment/install_choco_dependencies.ps1
@@ -1,5 +1,5 @@
 $scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Path
-$dependencies = Get-Content  ($scriptPath + "\dependencies.txt")
+$dependencies = Get-Content  ($scriptPath + "\choco_dependencies.txt")
 
 foreach($dep in $dependencies) {
     choco install -y $dep
