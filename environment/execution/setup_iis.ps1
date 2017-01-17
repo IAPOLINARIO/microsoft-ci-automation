7a90fabdb777b895c37cd24f0b8e8707cf990d7a
Ismael Apolinario <ismaelapolinario@MacBook-Pro-de-Ismael.local>
2017-01-17 10:53:50 -0200

    Removing IIS Management Tools from installation

--- environment/execution/setup_iis.ps1
+++ environment/execution/setup_iis.ps1
@@ -15,5 +15,4 @@
 
 Write-Host "Installing and configuring IIS 8.5..."
 import-module servermanager
-Add-WindowsFeature Web-Asp-Net, Web-Mgmt-Service, Web-Mgmt-Tools, Web-Server, Web-WebServer, Web-Common-Http, Web-Default-Doc, Web-Dir-Browsing, Web-Http-Errors, Web-Static-Content, Web-Health, Web-Http-Logging, Web-Performance, Web-Stat-Compression, Web-Security, Web-Filtering -IncludeManagementTools
-
+Add-WindowsFeature Web-Asp-Net, Web-Mgmt-Service, Web-Mgmt-Tools, Web-Server, Web-WebServer, Web-Common-Http, Web-Default-Doc, Web-Dir-Browsing, Web-Http-Errors, Web-Static-Content, Web-Health, Web-Http-Logging, Web-Performance, Web-Stat-Compression, Web-Security, Web-Filtering
